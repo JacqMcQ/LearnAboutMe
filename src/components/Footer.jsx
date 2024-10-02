ac4d30cb9d44ad326def7315e4c3256c1122dd16
JacMcQ <jacqlyn.mcquade@icloud.com>
2024-10-01 22:41:16 -0600

    added started code for all components and wired up using vite.config and react file roll-up structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <a
+          href="https://github.com/JacqMcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>{" "}
+        |{" "}
+        <a
+          href="www.linkedin.com/in/jacqlyn-mcquade"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
